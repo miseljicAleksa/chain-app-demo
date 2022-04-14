4db8f0e2c2648506fc9e05b8e48552919554370d
Aleksa <aleksa@Aleksas-MacBook-Pro.local>
2022-04-14 19:30:16 +0200

    be#1 - Login raw functionality

---
+++ backend/service/login.js
@@ -0,0 +1,61 @@
+const AWS = require('aws-sdk')
+AWS.config.update({
+    region: 'us-east-1'
+})
+const util = require('../utils/util')
+const bcrypt = require('bcryptjs')
+const auth = require('../utils/auth')
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const userTable = 'chainapp-demo'
+
+async function login(user) {
+    const {username, password} = user
+    
+    if(!user || !username || !password){
+        return util.buildResponse(401, {
+            message: 'username and password are required'
+        })
+    }
+
+    const dynamoUser = await getUser(username)
+    if(!dynamoUser || !dynamoUser.username){
+        return util.buildResponse(403, {
+            message: 'user does not exist'
+        })
+    }
+
+    if(!bcrypt.compareSync(password, dynamoUser.password)){
+        return util.buildResponse(403, {
+            message: 'password is incorrect'
+        })
+    }
+
+    const userInfo = {
+        username : dynamoUser.username,
+        name : dynamoUser.name 
+    }
+
+    const token = auth.generateToken(userInfo)
+    const response = {
+        user: userInfo,
+        token: token
+    }
+    return util.buildResponse(200, response)
+}
+
+async function getUser(username){
+    const params = {
+        TableName: userTable,
+        Key: {
+            username: username
+        }
+    }
+   return await dynamodb.get(params).promise().then(response => {
+       return response.Item
+   }, error => {
+       console.error('There is and error: ', error)
+   }) 
+}
+
+module.exports.login = login
